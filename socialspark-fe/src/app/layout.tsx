e986af00f1b9b28027b8e63efed14e00a7baca55
ViniciusFialhus <contatoviniciusfialhu@gmail.com>
2024-05-24 15:31:08 -0300

    fix: ajustado o favIcon

--- socialspark-fe/src/app/layout.tsx
+++ socialspark-fe/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="11zon_cropped.png" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
